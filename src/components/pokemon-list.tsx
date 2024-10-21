d28d9409ee4558c64348e76cbbec08c52ad6d67d
Barbara <barbarabarbabela@gmail.com>
2024-10-21 15:48:22 -0300

    feat: use pokemon query hook on pokemon list component

--- src/components/pokemon-list.tsx
+++ src/components/pokemon-list.tsx
@@ -1,32 +1,24 @@
 import React from 'react'
 import PokemonCard from './pokemon-card'
-import { useQuery } from '@tanstack/react-query'
-import { getPokemons } from '../http/get-pokemons'
-import { usePokemonContext } from '@/contexts/pokemon-context'
+import { usePokemonQuery } from '../hooks/use-pokemon-query'
 import { capitalizeFirstLetter } from '@/shared/capitalize-first-letter'
 import { formatPokemonNumber } from '@/shared/format-pokemon-id'
 
 function PokemonList() {
-  const { pokemon } = usePokemonContext()
-
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ['pokemon', pokemon],
-    queryFn: () => getPokemons(pokemon),
-    enabled: !!pokemon,
-  })
-
-  if (isLoading) {
-    return (
-      <div className="flex justify-center h-screen mt-9">
-        <p className="text-xl font-semibold animate-pulse">Carregando...</p>
-      </div>
-    )
-  }
+  const { data, isLoading, isError } = usePokemonQuery()
 
   if (isError) {
     return (
       <div className="flex justify-center h-screen mt-9">
         <p className="text-xl font-semibold">Erro ao buscar Pokémon</p>
+      </div>
+    )
+  }
+
+  if (isLoading) {
+    return (
+      <div className="flex justify-center h-screen mt-9">
+        <p className="text-xl font-semibold animate-pulse">Carregando...</p>
       </div>
     )
   }
